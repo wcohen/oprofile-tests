f8a53f859f7e94371eeb9dd63aee8a0e1e6c527a
Carl Love <cel@us.ibm.com>
2013-08-16 13:39:23 -0500

    Add test for the ocount tool
    The patch adds support for two additional testsuites for testing the ocount
    tool.  The first testsuite runs a series of tests on the ocount tool.  The
    tests include:
    
       - each event generates non-zero counts
       - counting for each event for an interval of n seconds, 2*n and 4*n scale as
         expected
       - the sum of the counts for each event in user mode only, kernel mode
         only match counting in user and kernel mode
       - the output using the brief output format is verified
    
    The above tests are run when the tool "oprofile" or "oprofile-ocount" are run.
    
    The second testsuite includes the additional test:
    
       - counting cycles for an interval of n seconds matches n*proc_frequency
    
    This testsuite is only run when the tool "cycle-check-ocount" is specified.
    The testsuite checks the event count against the reported CPU frequency which
    may or may not be the actual operating CPU frequency.  The reported frequency
    and the actual frequency may differ if the CPU is running in the Intel "turbo"
    mode or at a lower frequency for thermal management.  The user may need to
    manually verify any test failures based on the actual operating CPU frequency
    not the reported CPU frequency.
    
    Signed-off-by: Carl Love <cel@us.ibm.com>

---
+++ testsuite/workload_ocount/load.c
@@ -0,0 +1,81 @@
+/*  load.c
+ *   Copyright (C) 2013 IBM
+ *
+ *   Author: Carl Love  <carll@us.ibm.com>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sched.h>
+#include <link.h>
+
+#define ARRAY_SIZE      7000
+#define ARRAY_STEP      128/sizeof(int)
+
+#define REPORT_FREQ     100
+#define NUM_ITER        20000000
+#define UPPER_LIMIT     10000
+#define LOWER_LIMIT     10
+
+main(int argc, char *argv[])
+{
+
+  int array[ARRAY_SIZE];
+  int i, j;
+  int sum=0;
+  int direction=1;
+
+  int sum_add(int sum, int value);
+  int sum_sub(int sum, int value);
+
+  for (i=0; i<ARRAY_SIZE; i++) {
+    array[i] = i;
+  }
+
+  for (i=1; i<=NUM_ITER; i++) {
+    for (j=0; j<ARRAY_SIZE; j=j+ARRAY_STEP) {
+
+      if (sum > UPPER_LIMIT) {
+        direction = 0;
+      }
+
+      if (sum < LOWER_LIMIT) {
+        direction = 1;
+      }
+
+      if (direction == 1) {
+        sum=sum_add(sum, array[j]);
+      }
+
+      if (direction == 0) {
+        sum=sum_sub(sum, array[j]);
+      }
+    }
+  }
+}
+
+int sum_add(int sum, int value)
+{
+  return(sum+value);
+}
+
+int sum_sub(int sum, int value)
+{
+  return(sum-value);
+}
